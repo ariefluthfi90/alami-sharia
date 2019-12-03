e6b332d00cb9f58fe21b35a97aafd5d3f8da8b98
Ragil Wiradiputra <ragilwira@gmail.com>
2019-12-03 16:44:58 +0700

    fix sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+DELETE FROM seller_tbl;
+DELETE FROM product_tbl;
+
 INSERT INTO seller_tbl (id, created_date, modified_date, city, name) VALUES ('c0a8016d-6ec7-1865-816e-c799a8ae0000', '2019-12-03 00:12:46.477', '2019-12-03 00:12:46.477', 'Jakarta', 'Seller 1');
 INSERT INTO seller_tbl (id, created_date, modified_date, city, name) VALUES ('c0a8016d-6ec7-1865-816e-c79a0f940001', '2019-12-03 00:13:12.846', '2019-12-03 00:13:12.846', 'Jakarta', 'Seller 2');
 INSERT INTO seller_tbl (id, created_date, modified_date, city, name) VALUES ('c0a8016d-6ec7-1865-816e-c79a0f940002', '2019-12-03 00:13:12.846', '2019-12-03 00:13:12.846', 'Jakarta', 'Seller 3');
